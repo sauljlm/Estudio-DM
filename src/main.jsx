6d430d4e82a50b2ecb4730fe5ccffc9c876683d4
Saul Lopez <slopezm@ucenfotec.ac.cr>
2026-01-18 16:47:33 -0600

    I added a basic structure with some animations

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./tailwind.css";
+
+import Home from "./pages/Home";
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<Home />);
